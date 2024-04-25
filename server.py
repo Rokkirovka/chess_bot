23e74a9458693a8a873ef64528f3bb24ecff257d
Rokkirovka <adelgooglagel@gmail.com>
2024-04-25 23:08:45 +0500

    new functions

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 import random
+
 import chess
-import requests
 import chess.svg
+import requests
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters
-from io import BytesIO
 
 color_keyboard = [['белый', 'черный'],
                   ['случайно']]
 color_markup = ReplyKeyboardMarkup(color_keyboard, one_time_keyboard=True)
+BOT_TOKEN = '6605685633:AAHIAN7Zgwm1Nb_HoyAs81NwHYN33I2Thxc'
 
 
 def main():
@@ -25,7 +26,12 @@ def main():
                 CommandHandler('new_game', new_game)],
             4: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_move),
                 CommandHandler('help', help_command),
-                CommandHandler('print_board', print_board)]
+                CommandHandler('print_board', print_board),
+                CommandHandler('analysis', analysis),
+                CommandHandler('tip', tip),
+                CommandHandler('surrender', surrender)],
+            5: [CommandHandler('help', help_command),
+                CommandHandler('new_game', new_game)]
         },
         fallbacks=[CommandHandler('stop', stop)]))
     application.run_polling()
@@ -69,29 +75,86 @@ async def add_move(update, context):
     board = chess.Board(context.user_data['fen'])
     move = update.message.text
 
-    if board.is_legal(chess.Move.from_uci(move)):
-        commit_move(move, context)
-        move = get_moves(context.user_data['fen'], context.user_data['level'])
-        commit_move(move, context)
-        await update.message.reply_text(move)
-    else:
-        await update.message.reply_text('Некорректный ход. Чтобы посмотреть позицию, введите команду /print_board')
+    try:
+        if board.is_legal(chess.Move.from_uci(move)):
+            commit_move(move, context)
+            board = chess.Board(context.user_data['fen'])
+            if board.is_checkmate():
+                await update.message.reply_text('Мат! Вы хорошо сыграли!')
+                await update.message.reply_text('Если хотите сыграть ещё, напишите\n/new_game')
+                return 5
+            elif board.is_stalemate():
+                await update.message.reply_text('Пат. Ничья!')
+                await update.message.reply_text('Если хотите сыграть ещё, напишите\n/new_game')
+                return 5
+            move = get_moves(context.user_data['fen'], context.user_data['level'])
+            commit_move(move, context)
+            board = chess.Board(context.user_data['fen'])
+            await update.message.reply_text(move)
+            if board.is_checkmate():
+                await update.message.reply_text('Шах и мат!')
+                await update.message.reply_text('Если хотите сыграть ещё, напишите\n/new_game')
+                return 5
+            elif board.is_stalemate():
+                await update.message.reply_text('Пат. Ничья!')
+                await update.message.reply_text('Если хотите сыграть ещё, напишите\n/new_game')
+                return 5
+            elif board.is_check():
+                await update.message.reply_text('Вам шах!')
+        else:
+            await update.message.reply_text('Некорректный ход. Чтобы посмотреть позицию, введите команду\n/print_board')
+    except ValueError:
+        await update.message.reply_text('Неверно введен ход. Чтобы сходить, введите сначала начальную '
+                                        'клетку, а затем клетку, в которую вы хотите пойти.\nНапример: e2e4\n'
+                                        'Если вы ходите пешкой на последнюю горизонталь и хотите провести фигуру, '
+                                        'То после хода напишите символ фигуры.\nНапример: e7e8q')
 
 
 async def start(update, context):
-    await update.message.reply_text("Привет! Давай сыграем в шахматы! Чтобы начать новую игру, напиши /new_game.")
+    await update.message.reply_text("Привет! Давай сыграем в шахматы! Чтобы начать новую игру, напиши\n/new_game")
     return 1
 
 
 async def help_command(update, context):
-    await update.message.reply_text("Доступные команды: "
-                                    "/help"
-                                    "/board")
+    await update.message.reply_text("Доступные команды:\n"
+                                    "/help\n"
+                                    "/start\n"
+                                    "Во время партии:\n"
+                                    "/print_board - нарисовать доску\n"
+                                    "/tip - подсказка\n"
+                                    "/analysis - анализ партии\n"
+                                    "/surrender - сдаться")
 
 
 async def print_board(update, context):
     board = chess.Board(context.user_data['fen'])
-    await context.bot.send_photo(board.__str__())
+    await update.message.reply_text(board.__str__().replace('.', ' '))
+
+
+async def tip(update, context):
+    move = get_moves(context.user_data['fen'], 5)
+    await update.message.reply_text(f'Лучший ход в данной позиции - {move}')
+
+
+async def surrender(update, context):
+    await update.message.reply_text('Ты сделал все, что мог, и это уже большая победа.')
+    return 5
+
+
+async def analysis(update, context):
+    json = {'fen': context.user_data['fen'], 'depth': 5}
+    response = requests.get('http://127.0.0.1:5000/api/analysis', json=json).json()
+    score = response['score']
+    if '#' in score:
+        if '+' in score:
+            await update.message.reply_text(f'Белые могут поставить мат в {score[2:]} ходов.')
+        else:
+            await update.message.reply_text(f'Черные могут поставить мат в {score[2:]} ходов.')
+    else:
+        if '+' in score:
+            await update.message.reply_text(f'У белых преимущество на {int(score[1:]) / 100} сантипешек.')
+        else:
+            await update.message.reply_text(f'У черных преимущество на {int(score[1:]) / 100} сантипешек.')
 
 
 async def new_game(update, context):
@@ -107,8 +170,8 @@ async def stop(update, context):
 
 
 def get_moves(fen, level):
-    json = {'fen': fen}
-    response = requests.get('http://127.0.0.1/api/analysis', json=json).json()
+    json = {'fen': fen, 'depth': level}
+    response = requests.get('http://127.0.0.1:5000/api/analysis', json=json).json()
     return response['best_move']
 
 
